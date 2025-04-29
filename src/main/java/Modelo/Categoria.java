cc7bfe67845088ee3e6fdff9c9b84228d076bdb8
jgcamiloaga <jcamiloc148@gmail.com>
2025-04-29 18:11:10 -0500

    Agrega la clase Categoria para gestionar categorías de materiales.

---
+++ src/main/java/Modelo/Categoria.java
@@ -0,0 +1,32 @@
+package Modelo;
+
+public class Categoria {
+    
+    private String idCategoria;
+    private String nombre;
+
+    public Categoria() {
+    }
+
+    public Categoria(String idCategoria, String nombre) {
+        this.idCategoria = idCategoria;
+        this.nombre = nombre;
+    }
+
+    public String getIdCategoria() {
+        return idCategoria;
+    }
+
+    public void setIdCategoria(String idCategoria) {
+        this.idCategoria = idCategoria;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    
+}
