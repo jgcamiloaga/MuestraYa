7a370bbec0e9915aa384ec0c4a04f75abdb032ea
jgcamiloaga <jcamiloc148@gmail.com>
2025-06-02 18:27:18 -0500

    Agregar nuevos atributos y métodos en la clase Material para mejorar la gestión de materiales

--- src/main/java/modelo/dto/Material.java
+++ src/main/java/modelo/dto/Material.java
@@ -1,6 +1,7 @@
 package modelo.dto;
 
 import java.math.BigDecimal;
+import java.time.LocalDateTime;
 
 public class Material {
     private String idMaterial;
@@ -9,12 +10,22 @@ public class Material {
     private String idCategoria;
     private String nombreCategoria;
     private String imagen;
-
-    public Material(String idMaterial, String nombre, BigDecimal precio) {
+    private String descripcion;
+    private int stock;
+    private boolean destacado;
+    private String especificaciones;
+    private LocalDateTime fechaCreacion;
+    private String unidadMedida;
+    private BigDecimal peso;
+    private String dimension;
+    private String color;    public Material(String idMaterial, String nombre, BigDecimal precio) {
         this.idMaterial = idMaterial;
         this.nombre = nombre;
         this.precio = precio;
         this.imagen = "default.jpg";
+        this.stock = 0;
+        this.destacado = false;
+        this.fechaCreacion = LocalDateTime.now();
     }
 
     // Getters y setters existentes
@@ -61,9 +72,79 @@ public class Material {
     // Nuevo getter y setter para imagen
     public String getImagen() {
         return imagen;
-    }
-
-    public void setImagen(String imagen) {
+    }    public void setImagen(String imagen) {
         this.imagen = imagen;
     }
-}
+    
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+
+    public boolean isDestacado() {
+        return destacado;
+    }
+
+    public void setDestacado(boolean destacado) {
+        this.destacado = destacado;
+    }
+
+    public String getEspecificaciones() {
+        return especificaciones;
+    }
+
+    public void setEspecificaciones(String especificaciones) {
+        this.especificaciones = especificaciones;
+    }
+
+    public LocalDateTime getFechaCreacion() {
+        return fechaCreacion;
+    }
+
+    public void setFechaCreacion(LocalDateTime fechaCreacion) {
+        this.fechaCreacion = fechaCreacion;
+    }
+
+    public String getUnidadMedida() {
+        return unidadMedida;
+    }
+
+    public void setUnidadMedida(String unidadMedida) {
+        this.unidadMedida = unidadMedida;
+    }
+
+    public BigDecimal getPeso() {
+        return peso;
+    }
+
+    public void setPeso(BigDecimal peso) {
+        this.peso = peso;
+    }
+
+    public String getDimension() {
+        return dimension;
+    }
+
+    public void setDimension(String dimension) {
+        this.dimension = dimension;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+}    
